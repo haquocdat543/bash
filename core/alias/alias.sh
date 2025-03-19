37ee8ac31ef9dcf9bf6d9afee0b1852f51501236
haquocdat543 <wwwdatha543@gmail.com>
2025-03-19 15:03:24 +0700

    core(alias/cloud): add azure

--- core/alias/alias.sh
+++ core/alias/alias.sh
@@ -23,6 +23,7 @@ source "$(dirname "${BASH_SOURCE[0]}")/git/gitlab.sh"
 
 # Cloud
 source "$(dirname "${BASH_SOURCE[0]}")/cloud/openstack.sh"
+source "$(dirname "${BASH_SOURCE[0]}")/cloud/azure.sh"
 source "$(dirname "${BASH_SOURCE[0]}")/cloud/do.sh"
 
 # Infrastructure
