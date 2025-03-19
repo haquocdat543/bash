489f8805bcbc660c689cb123324423dd679b0c63
haquocdat543 <wwwdatha543@gmail.com>
2025-03-19 16:29:00 +0700

    core(alias/os): linux - add su

---
+++ core/alias/os/linux/su.sh
@@ -0,0 +1,7 @@
+### su
+alias su1='sudo su -'
+alias su2='su - ec2-user'
+alias su3='su - ubuntu'
+alias su4='su -'
+alias su5='su - postgres'
+alias su6='su - sonar'
--- core/alias/os/os.sh
+++ core/alias/os/os.sh
@@ -1,5 +1,6 @@
 # linux
 source "$(dirname "${BASH_SOURCE[0]}")/linux/basic.sh"
+source "$(dirname "${BASH_SOURCE[0]}")/linux/su.sh"
 source "$(dirname "${BASH_SOURCE[0]}")/linux/cd.sh"
 source "$(dirname "${BASH_SOURCE[0]}")/linux/cron.sh"
 source "$(dirname "${BASH_SOURCE[0]}")/linux/ufw.sh"
