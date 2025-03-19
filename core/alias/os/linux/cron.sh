3e0cb79e77ae9009e41ad6d0722ea9c9ff15f32b
haquocdat543 <wwwdatha543@gmail.com>
2025-03-19 16:24:36 +0700

    core(alias/os): linux - add cron

---
+++ core/alias/os/linux/cron.sh
@@ -0,0 +1,7 @@
+### Cron 
+alias ycron='ycr ; secr ; sscr'
+
+alias virc='vim /etc/cron.d/$CRONJOB'
+alias cre='crontab -e'
+alias crl='crontab -l'
+alias crr='crontab -r'
--- core/alias/os/os.sh
+++ core/alias/os/os.sh
@@ -1,6 +1,7 @@
 # linux
 source "$(dirname "${BASH_SOURCE[0]}")/linux/basic.sh"
 source "$(dirname "${BASH_SOURCE[0]}")/linux/cd.sh"
+source "$(dirname "${BASH_SOURCE[0]}")/linux/cron.sh"
 source "$(dirname "${BASH_SOURCE[0]}")/linux/iptables.sh"
 source "$(dirname "${BASH_SOURCE[0]}")/linux/systemctl.sh"
 source "$(dirname "${BASH_SOURCE[0]}")/linux/journalctl.sh"
