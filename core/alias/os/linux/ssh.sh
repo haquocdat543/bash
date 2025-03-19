5e788c7478562a75da3f9d86dd354db52e5d2fe8
haquocdat543 <wwwdatha543@gmail.com>
2025-03-19 16:37:02 +0700

    core(alias/os): linux - add ssh

---
+++ core/alias/os/linux/ssh.sh
@@ -0,0 +1,10 @@
+### SSH
+alias gh1='sshah ; gcnh ; gceh'
+alias gh2='sshac ; gcnc ; gcec'
+alias gh3='sshan ; gcnn ; gcen'
+alias sshsu="ssh ${osu}"
+alias ssha='eval $(ssh-agent) && ssh-add'
+alias ssha0='eval $(ssh-agent) && ssh-add $HOME/.ssh/Window2.pem'
+alias sshah='eval $(ssh-agent) && ssh-add $HOME/.ssh/id_ed25519'
+alias sshac='eval $(ssh-agent) && ssh-add $HOME/.ssh/id_rsa'
+alias sshan='eval $(ssh-agent) && ssh-add $HOME/.ssh/notional'
