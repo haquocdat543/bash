004edbd325c79c58e70d52bd6e70a5cba88d8e55
haquocdat543 <wwwdatha543@gmail.com>
2025-03-19 16:19:02 +0700

    core(alias/os): linux - add iptables

---
+++ core/alias/os/linux/iptables.sh
@@ -0,0 +1,6 @@
+### Iptables
+alias i7l='iptables -L -v -n --line-numbers'
+alias i7lnat='iptables -L -v -n --line-numbers -t nat'
+alias i7lsc='iptables -L -v -n --line-numbers -t security'
+alias i7lmg='iptables -L -v -n --line-numbers -t mangle'
+alias i7lraw='iptables -L -v -n --line-numbers -t raw'
--- core/alias/os/os.sh
+++ core/alias/os/os.sh
@@ -1,5 +1,6 @@
 # linux
 source "$(dirname "${BASH_SOURCE[0]}")/linux/basic.sh"
 source "$(dirname "${BASH_SOURCE[0]}")/linux/cd.sh"
+source "$(dirname "${BASH_SOURCE[0]}")/linux/iptables.sh"
 source "$(dirname "${BASH_SOURCE[0]}")/linux/systemctl.sh"
 source "$(dirname "${BASH_SOURCE[0]}")/linux/journalctl.sh"
