0501a642d49adb203ab7a9124ac2a9b8315a3f9a
haquocdat543 <wwwdatha543@gmail.com>
2025-03-19 15:32:05 +0700

    core(alias/navigation): add navi

--- core/alias/alias.sh
+++ core/alias/alias.sh
@@ -1,4 +1,5 @@
 # Navigation
+source "$(dirname "${BASH_SOURCE[0]}")/navigation/navi.sh"
 source "$(dirname "${BASH_SOURCE[0]}")/navigation/tmux.sh"
 source "$(dirname "${BASH_SOURCE[0]}")/navigation/screen.sh"
 source "$(dirname "${BASH_SOURCE[0]}")/navigation/clipboard.sh"
---
+++ core/alias/navigation/navi.sh
@@ -0,0 +1,9 @@
+## Navi
+alias n3='navi'
+alias n3h='navi --help'
+alias n3p='navi --print'
+alias n3r='navi repo'
+alias n3rb='navi repo browse'
+alias n3ra='navi repo add'
+alias cdn3l='cd $HOME/.local/share/navi'
+alias cdn3c='cd $HOME/.config/navi'
