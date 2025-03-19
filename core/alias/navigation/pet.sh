1468d341f039d6027b02a615aaa90d092b8a2bf2
haquocdat543 <wwwdatha543@gmail.com>
2025-03-19 15:32:41 +0700

    core(alias/navigation): add pet manager

---
+++ core/alias/navigation/pet.sh
@@ -0,0 +1,8 @@
+## Pet manager
+alias petl='pet list'
+alias pete='pet edit'
+alias petn='pet new'
+alias petex='pet exec'
+alias petcf='pet configure'
+alias petv='pet version'
+alias pets='pet sync'
