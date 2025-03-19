27b21c1f362ba7b6866b62d196a266bb77feb2c0
haquocdat543 <wwwdatha543@gmail.com>
2025-03-19 16:51:55 +0700

    core(alias/os): linux - add ls

---
+++ core/alias/os/linux/ls.sh
@@ -0,0 +1,23 @@
+### ls
+alias lr='ll ~'
+alias lll='ll /'
+alias ll='ls -lahFi --color'
+alias lpr='ls -la /proc'
+alias lh='ll /home'
+alias letc='ll /etc'
+alias lbin='ll /usr/local/bin'
+alias lgob='ll $HOME/go/bin'
+alias lrp='ll /etc/yum.repos.d'
+alias lssh='ll /root/.ssh'
+alias ldkimg='ll /var/lib/docker/overlay2'
+
+alias llg='. $HOME/preconfig/Suitcase/bash/scripts/llg.sh'
+alias llgc='. $HOME/preconfig/Suitcase/bash/scripts/llgc.sh'
+alias lbk='ll $HOME/preconfig/backup'
+alias ldl='ll $HOME/Downloads'
+alias ldc='ll $HOME/Documents'
+alias ldt='ll $HOME/Desktop'
+
+### Linux's network
+alias lnw='ls /sys/class/net'
+
