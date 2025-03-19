33578eca9c64042052cdbe22796d2d31d3e60ae3
haquocdat543 <wwwdatha543@gmail.com>
2025-03-19 16:03:39 +0700

    core(alias/os): linux - add journalctl

---
+++ core/alias/os/linux/journalctl.sh
@@ -0,0 +1,9 @@
+### Journalctl command
+alias jnl='journalctl'
+alias jnlb='journalctl -b'
+alias jnlu='journalctl -u'
+alias jnlp='journalctl -p'
+alias jnlf='journalctl -f'
+alias jnlsc='journalctl --since'
+alias jnlut='journalctl --until'
+alias jctlprm='journalctl -u prometheus -f --no-pager'
--- core/alias/os/os.sh
+++ core/alias/os/os.sh
@@ -1,2 +1,3 @@
 # linux
 source "$(dirname "${BASH_SOURCE[0]}")/linux/basic.sh"
+source "$(dirname "${BASH_SOURCE[0]}")/linux/journalctl.sh"
