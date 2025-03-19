68dff4a7108cef4151a261c35d2712df8b04dc42
haquocdat543 <wwwdatha543@gmail.com>
2025-03-19 16:26:42 +0700

    core(alias/os): linux - add ufw

---
+++ core/alias/os/linux/ufw.sh
@@ -0,0 +1,12 @@
+### ufw command
+alias ufwe='sudo ufw enable'
+alias ufwd='sudo ufw disable'
+alias ufws='sudo ufw status'
+alias ufwsn='sudo ufw status numbered'
+alias ufwa='sudo ufw allow'
+alias ufwassh='sudo ufw allow OpenSSH'
+alias ufwanxf="sudo ufw allow 'Nginx Full'"
+alias ufwdn='sudo ufw deny'
+alias ufwr='sudo ufw reset'
+alias ufwlm='sudo ufw limit'
+alias ufwlo='sudo ufw logging on'
--- core/alias/os/os.sh
+++ core/alias/os/os.sh
@@ -2,6 +2,7 @@
 source "$(dirname "${BASH_SOURCE[0]}")/linux/basic.sh"
 source "$(dirname "${BASH_SOURCE[0]}")/linux/cd.sh"
 source "$(dirname "${BASH_SOURCE[0]}")/linux/cron.sh"
+source "$(dirname "${BASH_SOURCE[0]}")/linux/ufw.sh"
 source "$(dirname "${BASH_SOURCE[0]}")/linux/iptables.sh"
 source "$(dirname "${BASH_SOURCE[0]}")/linux/systemctl.sh"
 source "$(dirname "${BASH_SOURCE[0]}")/linux/journalctl.sh"
