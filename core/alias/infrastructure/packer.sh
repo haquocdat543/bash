0cd78f1840eff7283cc2fb182e0514428aabe4f2
haquocdat543 <wwwdatha543@gmail.com>
2025-03-19 17:09:45 +0700

    core(alias/infrastructure): packer - add install plugin

--- core/alias/infrastructure/packer.sh
+++ core/alias/infrastructure/packer.sh
@@ -4,3 +4,5 @@ alias pkb='packer build'
 alias pkbc='packer build .'
 alias pkbv='packer build -var'
 alias pkpi='packer plugins install'
+alias pkpia='packer plugins install github.com/hashicorp/amazon'
+
