ac5fff50157a164098de477a2e5d0de684a50d05
haquocdat543 <wwwdatha543@gmail.com>
2025-03-19 14:46:53 +0700

    core(alias/programming): add java

---
+++ core/alias/programming/java.sh
@@ -0,0 +1,18 @@
+### Maven
+alias nvm='nvm'
+alias nvmci='mvn clean install'
+alias nvmv='mvn validate'
+alias nvmc='mvn compile'
+alias nvmt='mvn test'
+alias nvmp='mvn package'
+alias nvmi='mvn install'
+alias nvmd='mvn deploy'
+
+### Gradle
+alias gdl='gradle'
+alias gdli='gradle init'
+alias gdlb='gradle build'
+alias gdlt='gradle test'
+alias gdltk='gradle tasks'
+alias gdlc='gradle clean'
+alias gdla='gradle assemble'
