b254f783baf1802372acd18e5ef267ae819aea0c
haquocdat543 <wwwdatha543@gmail.com>
2025-03-19 16:31:18 +0700

    core(alias/os): linux - add rm

---
+++ core/alias/os/linux/rm.sh
@@ -0,0 +1,15 @@
+# rm
+alias rf='rm -rf'
+alias rfa='rm -rf *'
+alias rfaa='rm -rf * .*'
+alias rm='rm -f'
+alias rma='rm -f *'
+alias rmaa='rm -f * *.'
+
+alias dhis='rm $HOME/.bash_history'
+alias 25519del='rm -f  $HOME/.ssh/id_ed25519*'
+
+alias rmkh='rm $HOME/.ssh/known_hosts'
+alias rmkho='rm $HOME/.ssh/known_hosts.old'
+alias rmkha='rmkh ; rmkho'
+
--- core/alias/os/os.sh
+++ core/alias/os/os.sh
@@ -2,6 +2,7 @@
 source "$(dirname "${BASH_SOURCE[0]}")/linux/basic.sh"
 source "$(dirname "${BASH_SOURCE[0]}")/linux/su.sh"
 source "$(dirname "${BASH_SOURCE[0]}")/linux/cd.sh"
+source "$(dirname "${BASH_SOURCE[0]}")/linux/rm.sh"
 source "$(dirname "${BASH_SOURCE[0]}")/linux/cron.sh"
 source "$(dirname "${BASH_SOURCE[0]}")/linux/ufw.sh"
 source "$(dirname "${BASH_SOURCE[0]}")/linux/iptables.sh"
