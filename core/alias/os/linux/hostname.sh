db21ce135ed917d0be4506f74508492759a6d8c0
haquocdat543 <wwwdatha543@gmail.com>
2025-03-19 17:08:34 +0700

    core(alias/os): linux - add hostname

---
+++ core/alias/os/linux/hostname.sh
@@ -0,0 +1,15 @@
+### hostname
+alias hoi='hostname -i'
+alias hos='hostname -s'
+alias hs='hostnamectl status'
+
+### Uname
+alias unn='uname -n'
+alias unm='uname -m'
+alias unr='uname -r'
+alias uns='uname -s'
+alias unv='uname -v'
+alias unp='uname -p'
+
+### Host
+alias hstns='host -t ns'
