1a76d52f31fbc555945960d852d4867d47491631
haquocdat543 <wwwdatha543@gmail.com>
2025-03-20 09:59:55 +0700

    core(config): add k8s and fzf

---
+++ core/config/fzf.sh
@@ -0,0 +1,2 @@
+# [ -f ${HOME}/.fzf.bash ] && source "${HOME}/.fzf.bash"
+export FZF_CTRL_T_OPTS="--preview='bat --theme=\"Catppuccin Mocha\" --style=numbers --color=always {}' --bind shift-up:preview-page-up,shift-down:preview-page-down"
