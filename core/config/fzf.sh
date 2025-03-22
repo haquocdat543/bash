e9a52fa267be980df18048b5db1a0406ee25a923
haquocdat543 <wwwdatha543@gmail.com>
2025-03-22 18:08:29 +0700

    feat(core/config): fzf - add FZF CTRL R OPTS

--- core/config/fzf.sh
+++ core/config/fzf.sh
@@ -1,2 +1,4 @@
-# [ -f ${HOME}/.fzf.bash ] && source "${HOME}/.fzf.bash"
-export FZF_CTRL_T_OPTS="--preview='bat --theme=\"Catppuccin Mocha\" --style=numbers --color=always {}' --bind shift-up:preview-page-up,shift-down:preview-page-down"
+[ -f ${HOME}/.fzf.bash ] && source "${HOME}/.fzf.bash"
+export FZF_CTRL_T_OPTS="--preview='bat --style=numbers --color=always {}' --bind shift-up:preview-page-up,shift-down:preview-page-down"
+export FZF_CTRL_R_OPTS="--sort --exact --height=40% --preview='echo {}' --reverse"
+
