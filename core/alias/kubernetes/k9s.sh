53b338291af3d606ad9a534a51c3c9522336e052
haquocdat543 <wwwdatha543@gmail.com>
2025-04-20 20:46:56 +0700

    feat(core/alias): kubernetes - k9s - add namespace

--- core/alias/kubernetes/k9s.sh
+++ core/alias/kubernetes/k9s.sh
@@ -1,6 +1,7 @@
 ### K9s
 alias k9sr='k9s --readonly'
 alias k9srol='k9s --readonly'
+alias k9sn='k9s --namespace'
 alias k9si='k9s info'
 alias k9ctx='k9s --context'
 alias k9kcf='k9s --kubeconfig' # PATH/TO/KUBECONFIG/FILE
