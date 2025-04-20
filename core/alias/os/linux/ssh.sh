593cf72cd008a63449b971834563a9c9e525ee85
haquocdat543 <wwwdatha543@gmail.com>
2025-04-20 20:47:18 +0700

    feat(core/alias): os - linux - ssh - add gh7

--- core/alias/os/linux/ssh.sh
+++ core/alias/os/linux/ssh.sh
@@ -8,3 +8,8 @@ alias ssha0='eval $(ssh-agent) && ssh-add $HOME/.ssh/Window2.pem'
 alias sshah='eval $(ssh-agent) && ssh-add $HOME/.ssh/id_ed25519'
 alias sshac='eval $(ssh-agent) && ssh-add $HOME/.ssh/id_rsa'
 alias sshan='eval $(ssh-agent) && ssh-add $HOME/.ssh/notional'
+
+alias gh7='ssh7; gcu7; gce7'
+alias ssh7='eval $(ssh-agent) && ssh-add $HOME/.ssh/crystal'
+alias gcu7='git config --global user.name "tungvv"'
+alias gce7='git config --global user.email "tungvv@vbatechs.com"'
