3d2f829733c955aa31a51c5f178bb581dd460044
haquocdat543 <wwwdatha543@gmail.com>
2025-05-03 16:24:42 +0700

    feat(alias): add programming/dotnet

---
+++ core/alias/programming/dotnet.sh
@@ -0,0 +1,10 @@
+### [Dotnet]
+alias dntap='dotnet add package'
+alias dntrp='dotnet remove package'
+alias dntnc='dotnet new console'
+alias dntnw='dotnet new web'
+alias dntnwapi='dotnet new webapi'
+alias dntrs='dotnet restore'
+alias dntb='dotnet build'
+alias dntr='dotnet run'
+alias dntt='dotnet test'
