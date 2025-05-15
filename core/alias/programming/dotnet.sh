26ead347248e6d53d6a9a6a96b512a2cea50402b
haquocdat543 <wwwdatha543@gmail.com>
2025-05-15 10:47:36 +0700

    feat(core/alias): dotnet - add entity framework core

--- core/alias/programming/dotnet.sh
+++ core/alias/programming/dotnet.sh
@@ -1,4 +1,4 @@
-### [Dotnet]
+# [Dotnet]
 alias dntap='dotnet add package'
 alias dntrp='dotnet remove package'
 alias dntnc='dotnet new console'
@@ -8,3 +8,7 @@ alias dntrs='dotnet restore'
 alias dntb='dotnet build'
 alias dntr='dotnet run'
 alias dntt='dotnet test'
+
+# [EntityFrameworkCore]
+alias dntefma='dotnet ef migrations add' # InitialCreate
+alias dntefdu='dotnet ef database update'
