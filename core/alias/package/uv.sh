2cf79bb633d12d78c302a0cc327c114e4e03eb9e
haquocdat543 <wwwdatha543@gmail.com>
2025-06-07 18:34:53 +0700

    feat(package/uv): extend more

--- core/alias/package/uv.sh
+++ core/alias/package/uv.sh
@@ -1,4 +1,11 @@
 # [UV]
+alias uvi="uv init"
+alias uvac=". .venv/bin/activate"
+alias uvde=". .venv/bin/deactivate"
+
 alias uva="uv add"
 alias uvr="uv remove"
+
 alias uvru="uv run"
+alias uvt="uv tree"
+alias uvl="uv lock"
