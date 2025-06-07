537df36b0b6859cfc0181571e2cdabb0de609ca7
haquocdat543 <wwwdatha543@gmail.com>
2025-06-07 16:49:00 +0700

    [alias][package][nix] add search

--- core/alias/package/nix.sh
+++ core/alias/package/nix.sh
@@ -1,6 +1,11 @@
 ### Nix
+### Shell
 alias nishell='nix-shell ${HOME}/mine/product/nix/shell.nix'
 
+### Search
+alias nixs='nix search nixpkgs'
+
+### Env
 alias nixei='nix-env -i'
 alias nixeq='nix-env -q'
 alias nixeu='nix-env -u'
