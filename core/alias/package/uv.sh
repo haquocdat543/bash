e88da6c8e113b0c0e2666a479cb81178264aeff6
haquocdat543 <wwwdatha543@gmail.com>
2025-06-08 07:27:00 +0700

    feat(package/uv): add python

--- core/alias/package/uv.sh
+++ core/alias/package/uv.sh
@@ -7,6 +7,9 @@ alias uva="uv add"
 alias uvr="uv remove"
 
 alias uvp="uv pip"
+alias uvpy="uv python"
+alias uvpyl="uv python list"
+alias uvpyi="uv python install"
 
 alias uvru="uv run"
 alias uvt="uv tree"
