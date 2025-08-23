399b9048539f8b579f49352aaf8f60973919c88d
haquocdat543 <wwwdatha543@gmail.com>
2025-08-23 16:02:43 +0700

    feat(config): java - update

--- core/config/java.sh
+++ core/config/java.sh
@@ -1,2 +1,8 @@
-export JDTLS_JVM_ARGS="-javaagent:$HOME/.m2/repository/org/projectlombok/lombok/1.18.30/lombok-1.18.30.jar"
-
+export JDTLS_JVM_ARGS="
+  -javaagent:$HOME/.local/share/nvim/mason/packages/jdtls/lombok.jar
+  -Xms512m
+  -Xmx2g
+  -Declipse.application=org.eclipse.jdt.ls.core.id1
+  -Dosgi.bundles.defaultStartLevel=4
+  -Declipse.product=org.eclipse.jdt.ls.core.product
+"
