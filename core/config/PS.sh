845e00df9bdb79389846c8db665b57e70b972ae8
haquocdat543 <wwwdatha543@gmail.com>
2025-10-05 15:45:24 +0700

    feat(config/PS): update to use prompt command and git branch

--- core/config/PS.sh
+++ core/config/PS.sh
@@ -1,1 +1,8 @@
-PS1="[${red}\u${nc}@${yellow}\h${nc} ${green}\w${nc} ${cyan}{\#}${nc}] (\s \V)#"
+parse_git_branch() {
+	branch=$(git branch --show-current 2>/dev/null)
+	if [ -n "$branch" ]; then
+		echo "$branch"
+	fi
+}
+
+PROMPT_COMMAND='PS1="[${red}\u${nc}@${yellow}\h${nc} ${green}\w${nc} ${cyan}{\#}${nc}] (${cyan}$(parse_git_branch)${nc}) (\s \V)# "'
