a4d81f184d8e2d44385a659599141d95df867f92
haquocdat543 <wwwdatha543@gmail.com>
2025-12-23 18:36:02 +0700

    devcontainer: init

---
+++ core/alias/programming/devcontainer.sh
@@ -0,0 +1,7 @@
+alias devc1="devcontainer up --workspace-folder ."
+alias devc2="devcontainer up --workspace-folder . --remove-existing-container"
+alias devc3="devcontainer exec --workspace-folder . nvim"
+
+alias devc4="devcontainer up --config ~/bash/.devcontainer/devcontainer.json --workspace-folder ."
+alias devc5="devcontainer up --config ~/bash/.devcontainer/devcontainer.json --workspace-folder . --remove-existing-container"
+alias devc6="devcontainer exec --config ~/bash/.devcontainer/devcontainer.json --workspace-folder . nvim"
