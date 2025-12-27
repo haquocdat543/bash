07b0197650a8460ca9c77b3ea34dbfad47e41585
haquocdat543 <wwwdatha543@gmail.com>
2025-12-27 13:40:49 +0700

    alias(devcontainer): add devc devc7

--- core/alias/programming/devcontainer.sh
+++ core/alias/programming/devcontainer.sh
@@ -1,3 +1,5 @@
+alias devc="devcontainer exec --workspace-folder . "
+
 alias devc1="devcontainer up --workspace-folder ."
 alias devc2="devcontainer up --workspace-folder . --remove-existing-container"
 alias devc3="devcontainer exec --workspace-folder . nvim"
@@ -5,3 +7,5 @@ alias devc3="devcontainer exec --workspace-folder . nvim"
 alias devc4="devcontainer up --config ~/bash/.devcontainer/devcontainer.json --workspace-folder ."
 alias devc5="devcontainer up --config ~/bash/.devcontainer/devcontainer.json --workspace-folder . --remove-existing-container"
 alias devc6="devcontainer exec --config ~/bash/.devcontainer/devcontainer.json --workspace-folder . nvim"
+
+alias devc7="devcontainer exec --config ~/bash/.devcontainer/devcontainer.json --workspace-folder . "
