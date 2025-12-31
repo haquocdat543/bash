c3d7b820d124120a81eb8a9ccb9cd45525b3bbc4
haquocdat543 <wwwdatha543@gmail.com>
2025-12-31 06:14:40 +0000

    alias(conan): extend

--- core/alias/package/conan.sh
+++ core/alias/package/conan.sh
@@ -3,3 +3,10 @@ alias cnv='conan --version'
 alias cnh='conan --help'
 alias cni='conan install'
 alias cnpd='conan profile detect'
+
+alias cn1="conan profile detect"
+alias cn2="conan install . -s build_type=Release --build=missing"
+alias cn3="cmake -S . -B build -DCMAKE_TOOLCHAIN_FILE=build/Release/generators/conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXE_LINKER_FLAGS=\"-static\""
+alias cn4="cmake --build build"
+
+alias cn5="conan graph build-order"
