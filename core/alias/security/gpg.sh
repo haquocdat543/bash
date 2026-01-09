f367742a1ed9871c89a7965f52741dddf546e240
haquocdat543 <wwwdatha543@gmail.com>
2026-01-09 08:21:51 +0700

    alias(gpg): reorder

--- core/alias/security/gpg.sh
+++ core/alias/security/gpg.sh
@@ -1,13 +1,26 @@
 ### GPG key
-alias gpgae='gpg --armor --export'
-alias gpglk='gpg --list-keys'
-alias gpglsk='gpg --list-secret-keys'
-alias gpglkfl='gpg --list-secret-keys --keyid-format LONG'
+## Generate
 alias gpgfgk='gpg --full-generate-key'
-alias gpgpw='gpg --passwd'
-alias gpgim='gpg --import'
+
+## Export
+alias gpgae='gpg --armor --export'
 alias gpgex='gpg --export --output'
 alias gpgexsk='gpg --export-secret-keys --output'
+
+## Import
+alias gpgim='gpg --import'
+
+## List
+alias gpglk='gpg --list-keys'
+alias gpglkl='gpg --list-keys --keyid-format LONG'
+alias gpglsk='gpg --list-secret-keys'
+alias gpglkfl='gpg --list-secret-keys --keyid-format LONG'
+
+## Edit
+alias gpgpw='gpg --passwd'
 alias gpgek='gpg --edit-key'
+alias gpgeek='gpg --expert --edit-key' # this [addkey] command support [A] authentication capability
+
+## Delete
 alias gpgdk='gpg --delete-key'
 alias gpgdsk='gpg --delete-secret-key'
