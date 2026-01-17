6a1a647bb2ed6228cbcaefa2a0531e1b74f0170a
haquocdat543 <wwwdatha543@gmail.com>
2026-01-17 09:55:05 +0000

    script(install): add install-pkg-pnpm

--- core/alias/script/install.sh
+++ core/alias/script/install.sh
@@ -9,3 +9,4 @@ alias install-crypto-solana='. $HOME/bash/core/script/crypto/solana.sh'
 ### PKG
 alias install-pkg-bun='. $HOME/bash/core/script/pkg/bun.sh'
 alias install-pkg-nvm='. $HOME/bash/core/script/pkg/nvm.sh'
+alias install-pkg-pnpm='curl -fsSL https://get.pnpm.io/install.sh | sh -'
