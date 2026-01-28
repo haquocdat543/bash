d10473d41be56df5f419d93bee692bf8199a3b4f
haquocdat543 <wwwdatha543@gmail.com>
2026-01-28 06:14:49 +0000

    config: update mise

--- core/config/mise.sh
+++ core/config/mise.sh
@@ -1,2 +1,3 @@
 # if you face error during installing. Use this following command:
 # gpg --keyserver hkps://keyserver.ubuntu.com --recv-keys 5BE8A3F6C8A5C01D106C0AD820B1A390B168D356
+eval "$(~/.local/bin/mise activate)"
