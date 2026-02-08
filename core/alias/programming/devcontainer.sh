91cad10a4ee9b26054bbbecf06c9ce93bfc63267
haquocdat543 <wwwdatha543@gmail.com>
2026-02-08 01:10:33 +0000

    devcontainer: improve devc9

--- core/alias/programming/devcontainer.sh
+++ core/alias/programming/devcontainer.sh
@@ -14,15 +14,14 @@ alias devc8='cat ${HOME}/bash/.devcontainer/container/Dockerfile | docker build 
 
 alias devc9='
   docker run -d \
-    --mount type=bind,source=${HOME}/.vimrc,target=/root/.vimrc \
-    --mount type=bind,source=${HOME}/.config/nvim,target=/root/.config/nvim \
-    --mount type=bind,source=${HOME}/.bashrc,target=/root/.bashrc \
-    --mount type=bind,source=${HOME}/bash,target=/root/bash \
-    --mount type=bind,source=$(pwd),target=/workspace/$(basename $(pwd)) \
+    --privileged \
+	  --cap-add=SYS_PTRACE \
+    --security-opt seccomp=unconfined \
+    --mount type=volume,source=dind-${NAME}-data,target=/var/lib/docker \
     --name ${NAME} \
-    devcontainer:v2
+    devcontainer:v1 \
+    /bin/sh -c "dockerd > /tmp/dockerd.log 2>&1 & sleep infinity"
 '
-
 alias devc10='
   docker run -d \
     --privileged \
