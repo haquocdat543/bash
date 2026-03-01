6275e162e27cb247e55d4a4fc1ea0ce9e70b4cd2
haquocdat543 <wwwdatha543@gmail.com>
2026-03-01 14:37:46 +0000

    package(bun): add bunnext

--- core/alias/package/bun.sh
+++ core/alias/package/bun.sh
@@ -4,3 +4,5 @@ alias buna='bun add'
 alias bunad='bun add -d'
 alias bunu='bun update'
 alias bunr='bun remove'
+
+alias bunnext='bunx create-next-app@latest . --typescript --eslint --tailwind --app --src-dir --import-alias "@/*"'
