d667168cd21a21e8c2e77faec3be3c18904e6160
haquocdat543 <wwwdatha543@gmail.com>
2026-03-14 14:05:10 +0700

    crypt: add lock force

--- core/alias/git/crypt.sh
+++ core/alias/git/crypt.sh
@@ -2,4 +2,5 @@ alias gcri="git-crypt init"
 alias gcra="git-crypt add-gpg-user" # FINGERPRINT
 alias gcrs="git-crypt status"
 alias gcrl="git-crypt lock"
+alias gcrlf="git-crypt lock --force"
 alias gcru="git-crypt unlock"
