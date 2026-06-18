686e8c5a6c62e7eea609de1f70fdd898f479d3ea
haquocdat543 <wwwdatha543@gmail.com>
2026-06-18 21:04:00 +0700

    config: improve mise

--- core/config/mise.sh
+++ core/config/mise.sh
@@ -1,3 +1,5 @@
 # if you face error during installing. Use this following command:
 # gpg --keyserver hkps://keyserver.ubuntu.com --recv-keys 5BE8A3F6C8A5C01D106C0AD820B1A390B168D356
 eval "$(~/.local/bin/mise activate)"
+export PATH="${PATH}:/root/.local/bin"
+export PATH="${PATH}:/root/.local/share/mise/shims"
